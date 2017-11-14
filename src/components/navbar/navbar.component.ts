279e6739b42cfca62dc32d7ee08b292e125ca3ff
Gyorgy Balassy <Gyorgy.Balassy@logmein.com>
2017-11-14 17:43:27 +0100

    ADDED: Exit from the application with the navbar button.

--- src/components/navbar/navbar.component.ts
+++ src/components/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { remote } from 'electron';
 
 @Component({
   selector: 'app-navbar',
@@ -6,4 +7,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./components/navbar/navbar.component.css']
 })
 export class NavbarComponent {
+  public onCloseButtonClicked(): void {
+    const currentWindow: Electron.BrowserWindow = remote.BrowserWindow.getFocusedWindow();    
+    
+    const options: Electron.MessageBoxOptions = {
+      title: 'Exit',
+      type: 'warning',
+      buttons: [ 'OK', 'Cancel' ],
+      message: 'Are you sure you want to exit the application?'
+    };
+
+    remote.dialog.showMessageBox(currentWindow, options, (response: number) => {
+      if (response === 0) {
+        currentWindow.close();
+      }
+    });
+  }
 }
